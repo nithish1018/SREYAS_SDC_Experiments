119f85aa742bf97cfe32c41057637c8666245bb2
Yogesh Bhutkar <pseudonetwork1001@gmail.com>
2023-04-18 14:23:16 +0530

    Added exp 13

---
+++ Exp 13/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css";
+import Templ from './components/Templ';
+
+const App = () => {
+  return (
+    <>
+    <Templ/>
+    </>
+  )
+}
+
+export default App
